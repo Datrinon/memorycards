218940bbe0cffa1fe665fd4d3818c1f3283989d5
Datrinon <dtrinh089@gmail.com>
2021-10-21 16:13:59 -0700

    Add dummy functionality to Game to test out a level; basic game loop (losing) now works.
    Dummy images:
    burger chicken cola
    are used to establish some fake DUMMY_CARDS. DUMMY_CARDS_STATUS was also created too to keep track of whether or not any of those cards had been selected. The user selects a card, which marks that as selected (after checking if it had been selected; that case would mean game over), and increases their score.
    
    However, still need to code in the win logic.

--- src/js/Models/GameState.js
+++ src/js/Models/GameState.js
@@ -1,7 +1,6 @@
 const GAME_STATE = {
   MENU: "menu",
   PLAYING: "playing",
-  ROUND_WIN: "round_win",
   GAMEOVER_WIN: "gameover_win",
   GAMEOVER_LOSE: "gameover_lose"
 }
--- src/js/components/Game.js
+++ src/js/components/Game.js
@@ -1,23 +1,162 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Loading from "./Loading";
+import MemoryCard from './MemoryCard';
+
+import GAME_STATE from '../Models/GameState';
+
+import { cloneDeep } from 'lodash';
+
+// dummy data imports, remove later.
+import Burger from "../../images/burger01.png";
+import Chicken from "../../images/chicken01.png";
+import Cola from "../../images/cola01.png";
+import Utility from '../Util/utility';
+
+import useScore from '../hooks/useScore';
+
+
+const DUMMY_CARDS = [
+  <MemoryCard 
+    id={0}
+    key={Burger}
+    src={Burger}
+  />,
+  <MemoryCard
+    id={1}
+    key={Chicken}
+    src={Chicken}
+  />,
+  <MemoryCard
+    id={2}
+    key={Cola}
+    src={Cola}
+  />
+]
+
+const DUMMY_CARDS_STATUS = [
+  {
+    id: 0,
+    pressed: false,
+  },
+  {
+    id: 1,
+    pressed: false,
+  },
+  {
+    id: 2,
+    pressed: false
+  }
+]
+
+const DUMMY_STATE_READY = true;
+
 
 
 function Game(props) {
-  const [playerReady, setPlayerReady] = useState(false);
+  // debugger;
+  const [playerReady, setPlayerReady] = useState(DUMMY_STATE_READY);
   const [currentLevel, setCurrentLevel] = useState(0);
-  const [score, setScore] = useState(0);
-  const [memoryCards, setMemoryCards] = useState(null);
+  const [score, setScore] = useScore(0);
+  const [memoryCards, setMemoryCards] = useState(cloneDeep(DUMMY_CARDS));
+  const [memoryCardsStatus, setMemoryCardsStatus] = useState(cloneDeep(DUMMY_CARDS_STATUS));
+  const [playerWonRound, setPlayerWonRound] = useState()
+
+  console.log({playerReady, currentLevel, score, memoryCards, memoryCardsStatus})
 
   function receiveMemoryCards(cards) {
-    console.log(JSON.stringify(cards));
     setMemoryCards(cards);
+
+    let status = memoryCardsStatus.map((elem, index) => {
+      return {
+        id: elem.id,
+        pressed: false
+      }
+    });
+
+    setMemoryCardsStatus(status);
   }
 
+  function handleMemoryCardClick(event) {
+    console.log("click called.");
+    // mark element as selected & update state
+    let card = Utility.getMatchingParent(event.target, ".memory-card");
+
+    let selected = parseInt(card.dataset.key);
+    let lost = false;
+    let newMemoryCardsStatus = memoryCardsStatus.map((elem, index) => {
+      if (elem.id === selected) {
+        // if pressed is already true here then we send the game to a game over state.
+        if (elem.pressed) {
+          lost = true;
+        } else {
+          elem.pressed = true;
+          // setScore(score.current + 1);
+          setScore(score + 1);
+        }
+
+      }
+      return elem;
+    })
+
+    if (lost) {
+      props.setGameState(GAME_STATE.GAMEOVER_LOSE);
+      return null;
+    }
+
+    setMemoryCardsStatus(newMemoryCardsStatus);
+
+    // now reshuffle the elements (from Fisher Yates)
+    shuffleCards();
+  }
+
+  function shuffleCards() {
+    let updatedMemoryCards = [...memoryCards];
+    let currentIndex = memoryCards.length;
+    let randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // destructure
+      [updatedMemoryCards[currentIndex], updatedMemoryCards[randomIndex]] =
+          [updatedMemoryCards[randomIndex], updatedMemoryCards[currentIndex]];
+    }
+
+    // TODO
+    // DEBUG
+    // Remove this later
+    updatedMemoryCards = updatedMemoryCards.map((elem) => {
+      return React.cloneElement(
+        elem,
+        {onClick: handleMemoryCardClick}
+      );
+    });
+
+    setMemoryCards(updatedMemoryCards);
+  }
 
   function playerIsReady() {
     setPlayerReady(true);
   }
 
+  
+  // TODO
+  // Dummy useEffect, make sure to remove this later
+  useEffect(() => {
+    const withHandlers = DUMMY_CARDS.map(elem => {
+      return React.cloneElement(
+        elem,
+        {onClick: handleMemoryCardClick}
+      )
+    });
+
+    setMemoryCards(withHandlers);
+
+    return () => {
+      console.log("game component unmounted.")
+    }
+  }, []);
 
   if (!playerReady) {
     return (
@@ -26,12 +165,14 @@ function Game(props) {
         numCardsForLevel={props.levels[currentLevel]}
         playerReady={playerIsReady}
         passBackMemoryCards={receiveMemoryCards}
+        memoryCardOnClick={handleMemoryCardClick}
       />
     )
   }
 
   return (
     <div>
+      <p>Score: {score.current}</p>
       The game starts here.
       {memoryCards}
     </div>
