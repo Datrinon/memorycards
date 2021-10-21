ccdc073b208797b60b4d4d660bc1d66077698b6c
Datrinon <dtrinh089@gmail.com>
2021-10-20 21:30:04 -0700

    Add Game State Enum Object

---
+++ src/js/Models/GameState.js
@@ -0,0 +1,9 @@
+const GAME_STATE = {
+  MENU: "menu",
+  PLAYING: "playing",
+  ROUND_WIN: "round_win",
+  GAMEOVER_WIN: "gameover_win",
+  GAMEOVER_LOSE: "gameover_lose"
+}
+
+export default GAME_STATE;
