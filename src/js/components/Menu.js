6c79dd04525ca86647e67b211e3e5181e9c5cf6b
Datrinon <dtrinh089@gmail.com>
2021-10-23 13:52:16 -0700

    CSS: Add Menu animations when buttons pressed

--- src/js/components/Menu.js
+++ src/js/components/Menu.js
@@ -3,24 +3,32 @@ import "../../css/Menu.css";
 
 
 function Menu(props) {
-  const [animation, setAnimation] = useState(0);
+  const [playAnimation, setPlayAnimation] = useState(0);
+  const [checkboxAnimation, setCheckboxAnimation] = useState(0);
   
   function handleCheckboxClick(event) {
     props.toggleHardMode(event);
-    setAnimation((prevAnimation) => {
+    setCheckboxAnimation((prevAnimation) => {
       return prevAnimation === 1 ? 0 : 1;
     });
   }
 
+  function handlePlayButtonClick(event) {
+    setPlayAnimation(1);
+    document.querySelector('.menu').addEventListener("animationend", () => {
+      props.startGame(event);
+    })
+  }
+
   return (
-    <div className="menu">
+    <div className="menu" animation={playAnimation}>
       <h1 className="game-title">Memory Game</h1>
-      <button onClick={props.startGame} className="play-button">Play!</button>
+      <button onClick={handlePlayButtonClick} className="play-button">Play!</button>
       <label className="enable-hard">
         <input type="checkbox" onClick={handleCheckboxClick}></input>
         Enable Hard Mode
       </label>
-      <p className="difficulty-caption" animation={animation}>You'll get <span className="seconds">{props.timePerCard} seconds</span> per card.</p>
+      <p className="difficulty-caption" animation={checkboxAnimation}>You'll get <span className="seconds">{props.timePerCard} seconds</span> per card.</p>
     </div>
   )
 }
