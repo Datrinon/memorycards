eb33aa6d5905b4e41bc8f35239320b0199bac569
Datrinon <dtrinh089@gmail.com>
2021-10-24 10:37:00 -0700

    Fix CSS to make memory cards be even
    - Memory cards have even height and width
    - Memory cards have transitions when hovering over them.

--- src/js/components/MemoryCard.js
+++ src/js/components/MemoryCard.js
@@ -2,7 +2,8 @@ import "../../css/MemoryCard.css"
 
 function MemoryCard(props) {
   let matches = props.src.split("/");
-  let name = matches[matches.length - 1].split(/0-9+/)[0];
+  let name = matches[matches.length - 1].split(/[0-9]/)[0];
+  name = name.replace("-", " ");
 
   return (
   <figure data-key={props.id} className="memory-card" onClick={props.onClick}>
